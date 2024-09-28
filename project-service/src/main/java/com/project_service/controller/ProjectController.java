09819411b9332e478ed1657ed2aa0633474d96d8
kettaabii <elkettabimohammedamin@gmail.com>
2024-09-29 00:30:11 +0100

    autocomplete suggestion using elasticsearch in projects

--- project-service/src/main/java/com/project_service/controller/ProjectController.java
+++ project-service/src/main/java/com/project_service/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.project_service.controller;
 
 import com.project_service.dto.ProjectDto;
+import com.project_service.enums.Status;
 import com.project_service.exception.ProjectNotFoundException;
 import com.project_service.service.ProjectService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -74,4 +78,31 @@ public class ProjectController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
+
+    @GetMapping("/filter")
+    public ResponseEntity<?> filterProjects(
+            @RequestParam(required = false, name = "location") String location,
+            @RequestParam(required = false, name = "status") Status status,
+            @RequestParam(required = false, name = "minBudget") Double minBudget,
+            @RequestParam(required = false, name = "maxBudget") Double maxBudget,
+            @RequestParam(required = false, name = "dateStart") Date dateStart,
+            @RequestParam(required = false, name = "dateEnd") Date dateEnd,
+            @RequestParam(defaultValue = "0", name = "page") int page,
+            @RequestParam(defaultValue = "10", name = "size") int size,
+            @RequestParam(defaultValue = "name", name = "sortField") String sortField,
+            @RequestParam(defaultValue = "asc", name = "sortDirection") String sortDirection) {
+        try {
+            var projects = projectService.multiparamFiler(location, status, minBudget, maxBudget, dateStart, dateEnd, page, size, sortField, sortDirection);
+            return ResponseEntity.ok(projects);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/autocomplete")
+    public ResponseEntity<List<String>> autocomplete(@RequestParam( name = "term") String term) {
+        List<String> suggestions = projectService.getAutocompleteSuggestions(term);
+        return ResponseEntity.ok(suggestions);
+    }
+
 }
--- project-service/src/main/java/com/project_service/service/ProjectService.java
+++ project-service/src/main/java/com/project_service/service/ProjectService.java
@@ -2,11 +2,13 @@ package com.project_service.service;
 
 import com.project_service.client.RequestContext;
 import com.project_service.dto.ProjectDto;
+import com.project_service.enums.Status;
 import com.project_service.exception.ProjectNotFoundException;
 import com.project_service.mapper.ProjectMapper;
 import com.project_service.model.Project;
 import com.project_service.repository.ProjectRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +17,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+
 
 @Service
 @Transactional
@@ -96,4 +103,54 @@ public class ProjectService {
         return "Task service is temporarily unavailable. Please try again later.";
     }
 
+    public Page<Project> multiparamFiler(String location , Status status , Double minBudget , Double maxBudget , Date dateStart , Date dateEnd , int page , int size , String sortField , String sortDirection) {
+        Sort.Direction direction = sortDirection.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortField));
+
+        Specification<Project> spec =(root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (status != null) {
+                predicates.add(criteriaBuilder.equal(root.get("status"), status));
+            }
+            if (minBudget != null) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("budget"), minBudget));
+            }
+            if (maxBudget != null) {
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("budget"), maxBudget));
+            }
+            if (dateStart != null) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("date"), dateStart));
+            }
+            if (dateEnd != null) {
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("date"), dateEnd));
+            }
+            if (page > 0) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("page"), page));
+            }
+            if (size > 0) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("size"), size));
+            }
+            if (sortField != null) {
+                predicates.add(criteriaBuilder.equal(root.get("sortField"), sortField));
+            }
+            if (location != null) {
+                predicates.add(criteriaBuilder.equal(root.get("location"), location));
+            }
+            assert query != null;
+            query.orderBy(criteriaBuilder.asc(root.get("date")));
+
+            query.distinct(false);
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+        return projectRepository.findAll(spec,pageable);
+
+    }
+
+    public List<String> getAutocompleteSuggestions(String query) {
+        PageRequest pageable = PageRequest.of(0, 10);
+        Page<Project> results=projectRepository.autocomplete(query,pageable);
+        return results.getContent().stream()
+                .map(Project::getName)
+                .collect(Collectors.toList());
+    }
 }
